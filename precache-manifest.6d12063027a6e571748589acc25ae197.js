31c16c830ddf39b93559ed109c5f5b8f8884354a
Tatiana <eyesdwn@gmail.com>
2020-01-30 17:59:52 +0200

    Updates

---
+++ precache-manifest.6d12063027a6e571748589acc25ae197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8ac82540471903d0751304cdc44333",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "461aa1b6103c2c92638d",
+    "url": "/goit-react-hw-02-feedback/static/css/main.8260cfc6.chunk.css"
+  },
+  {
+    "revision": "cde625bb4a8279a3f618",
+    "url": "/goit-react-hw-02-feedback/static/js/2.96d4eb17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.96d4eb17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "461aa1b6103c2c92638d",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fbab1706.chunk.js"
+  },
+  {
+    "revision": "a9008001ac811cd62d60",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.58c7c2ad.js"
+  }
+]);
